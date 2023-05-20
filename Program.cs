60b2d9f64083dc1d9f40177fe1154f3b9e9a1ea1
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-20 17:36:36 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace UC2RANDOMDIEROLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to random dieroll");
+            int position = 0;
+            Random rand = new Random();
+            int dieRoll = rand.Next(1, 7);
+            Console.WriteLine("Welcome to Snake and Ladder game!");
+            Console.WriteLine($"position:{position}");
+            Console.WriteLine("Roll the dice (press any key to continue)");
+            Console.ReadLine();
+            Console.WriteLine("The player rolled a " + dieRoll);
+        }
+    }
+}
