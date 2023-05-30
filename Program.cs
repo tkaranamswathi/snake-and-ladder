7909af787cc748e5a09d828e78db4f260ecad1e9
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-30 16:12:00 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace UC4WINNINGPOS
+{
+    internal class Program
+    {
+            public static void Main(string[] args)
+            {
+                int winningPosition = 100;
+                int playerPosition = 0;
+
+                Random random = new Random();
+
+                while (playerPosition < winningPosition)
+                {
+                    int diceRoll = random.Next(1, 7); // Simulating a dice roll (random number between 1 and 6)
+                    playerPosition += diceRoll;
+
+                    if (playerPosition < 0)
+                    {
+                        playerPosition = 0;
+                    }
+
+                    Console.WriteLine($"Player position: {playerPosition}");
+
+                    if (playerPosition == winningPosition)
+                    {
+                        Console.WriteLine("Congratulations! You've reached the winning position.");
+                    }
+                    else if (playerPosition > winningPosition)
+                    {
+                        Console.WriteLine("Oops! You've gone past the winning position.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Roll the dice again.");
+                    }
+
+                    Console.WriteLine("Press Enter to roll the dice.");
+                    Console.ReadLine();
+                }
+            }
+    }
+}
+
+    
