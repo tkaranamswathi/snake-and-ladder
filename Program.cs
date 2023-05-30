7433c5ff0aea42c918cb3b3b99b41e9f78f255e4
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-30 16:51:46 +0530

    [adding welcome message]

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace UC7PLAYER1AND2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int winningPosition = 100;
+            int[] playerPositions = new int[2];
+            int currentPlayer = 0;
+            int diceRollCount = 0;
+
+            Random random = new Random();
+
+            while (playerPositions[currentPlayer] != winningPosition)
+            {
+                int diceRoll = random.Next(1, 7); // Simulating a dice roll (random number between 1 and 6)
+                int newPosition = playerPositions[currentPlayer] + diceRoll;
+
+                if (newPosition <= winningPosition)
+                {
+                    playerPositions[currentPlayer] = newPosition;
+                }
+
+                diceRollCount++;
+
+                Console.WriteLine($"Player {currentPlayer + 1}: Dice roll: {diceRoll}");
+                Console.WriteLine($"Player {currentPlayer + 1}: Position: {playerPositions[currentPlayer]}");
+
+                if (playerPositions[currentPlayer] == winningPosition)
+                {
+                    Console.WriteLine($"Player {currentPlayer + 1} won the game!");
+                    break;
+                }
+                else if (diceRoll == 6)
+                {
+                    Console.WriteLine("Ladder! Play again.");
+                }
+                else
+                {
+                    currentPlayer = (currentPlayer + 1) % 2; // Switch to the next player
+                }
+
+                Console.WriteLine("Press Enter to roll the dice.");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total dice rolls: {diceRollCount}");
+        }
+    }
+}
